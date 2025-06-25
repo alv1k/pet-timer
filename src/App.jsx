da6ebc67823b1f7686f7812df07ab1df2c8d4ac4
al_v1k <alekseevaalena442@gmail.com>
2025-06-25 15:24:31 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './index.css'
+import ThemeButton from './components/ThemeButton.jsx'
+import { PatternFormat } from 'react-number-format';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="h-[100vh] flex flex-col relative">
+      <div className="absolute top-0 right-0 p-12">
+        <ThemeButton />
+      </div>
+      <div className="w-fit m-auto">
+        <div className="flex flex-col gap-2">
+          <h1>Set timer</h1>
+          <PatternFormat value="000000" className="rounded-md bg-amber-300 text-amber-950 p-3 text-3xl" format="##:##:##" allowEmptyFormatting mask="_" />
+        </div>
+        <div className="mt-12">
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
