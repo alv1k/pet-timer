08f91a8c237214535f32b17256dffdab447b1e51
alv1k <alekseevaalena442@gmail.com>
2025-07-06 21:34:58 +0900

    transition3

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,22 @@ import ThemeButton from './components/ThemeButton.jsx'
 import { PatternFormat } from 'react-number-format';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const value = '000500';
+  const [timerStarted, setTimerStatus] = useState(false);
+  let amount = 300;
+  const startTimer = () => {
+    console.log('startTimer');
+    if(!timerStarted) {
+      setTimerStatus(true);
+      let timer = setInterval(amount--, 1000)
+    } else {
+      setTimerStatus(false)
+      
+    }
+    console.log(amount);
+
+  }
 
   return (
     <div className="h-[100vh] flex flex-col relative">
@@ -13,24 +28,20 @@ function App() {
       </div>
       <div className="w-fit m-auto">
         <div className="flex flex-col gap-2">
-          <h1>Set timer</h1>
+          <h1 className="cursor-default">Set timer</h1>
           <PatternFormat 
-            value="000500"
-            className="rounded-md bg-amber-300 text-amber-950 p-3 text-3xl" 
+            value={value}
+            className="rounded-md bg-amber-300 text-amber-950 p-3 text-3xl hover:bg-amber-200 active:bg-amber-100 cursor-pointer" 
             format="##:##:##"
             allowEmptyFormatting
             mask="_"
           />
         </div>
-        <div className="mt-12">
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-          </div>
-        </div>
-        <button className="p-3 bg-green">
-          Start
+        <button 
+          onClick={() => startTimer(value)} 
+          className="mt-5 w-1/2  p-3 bg-green-700 rounded-xl hover:bg-green-600 active:bg-green-500 cursor-pointer"
+        >
+          {timerStarted ? 'Stop' : 'Start'}
         </button>
       </div>
     </div>
