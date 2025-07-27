076036fde59622f2aeb1c9c917ce5d1d34248f04
alv1k <alekseevaalena442@gmail.com>
2025-07-27 12:53:02 +0900

    beauti

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './index.css'
 import ThemeButton from './components/ThemeButton.jsx'
 import { PatternFormat } from 'react-number-format';
@@ -12,7 +12,10 @@ function App() {
   const [seconds, setSeconds] = useState(0);
 
   
-  const [isSettingTime, setIsSettingTime] = useState(false);
+  const hoursRef = useRef(null);
+  const minutesRef = useRef(null);
+  const secondsRef = useRef(null);
+  
   const [customHours, setCustomHours] = useState('00');
   const [customMinutes, setCustomMinutes] = useState('00');
   const [customSeconds, setCustomSeconds] = useState('00');
@@ -68,19 +71,18 @@ function App() {
     
     setTimeLeft(totalSeconds);
     setIsRunning(true);
-    setIsSettingTime(false);
   };
   
-  const handleInputChange = (e, setter) => {
-
+  const handleInputChange = (e, setter, max) => {
     let value = e.target.value.replace(/\D/g, '').slice(0, 2);
 
-    if (value === '') {
-      value = '00';
-    } else if (value.length === 1) {
-      value = value.padStart(2, '0');
+    let num = parseInt(value || '0', 10);
+    if (num > max) {
+      num = max;
     }
-    console.log(value, 'value000');
+    
+    const formattedValue = num.toString().padStart(2, '0');
+  
     setter(value);
   };
 
@@ -90,72 +92,72 @@ function App() {
         <ThemeButton />
       </div>
       
-      <div className="w-1/7 m-auto">
-        <div>
-          <h1>settings</h1>
-          <div className="border rounded justify-center flex flex-row gap-2 mb-4 py-2">
-            <div>
+      <div className="2xl:w-1/7 xl:w-1/6 md:w-2/7 w-5/6 m-auto">
+        <div className="text-center">
+          <h1 className="pb-5">Set interval</h1>
+          <div className="border rounded justify-center flex gap-2 mb-4 py-2">
+            <div className="text-center">
               <label className="block text-sm">Hours</label>
               <input 
                 type="text" 
                 value={customHours}
-                onChange={(e) => handleInputChange(e, setCustomHours)}
-                className=" p-2 w-16"
+                onChange={(e) => handleInputChange(e, setCustomHours, 23)}
+                className="p-2 w-16 text-center rounded-md focus:outline-0 focus:shadow-md"
                 maxLength={2}
+                ref={hoursRef}
+                onFocus={() => hoursRef.current.select()} 
               />
             </div>
             <span className="self-end pb-2">:</span>
             
-            <div>
+            <div className="text-center">
               <label className="block text-sm">Minutes</label>
               <input 
                 type="text" 
                 value={customMinutes}
-                onChange={(e) => handleInputChange(e, setCustomMinutes)}
-                className=" p-2 w-16"
+                onChange={(e) => handleInputChange(e, setCustomMinutes, 59)}
+                className=" p-2 w-16 text-center rounded-md focus:outline-0 focus:shadow-md"
                 maxLength={2}
+                ref={minutesRef}
+                onFocus={() => minutesRef.current.select()} 
               />
             </div>
             <span className="self-end pb-2">:</span>
             
-            <div>
+            <div className="text-center">
               <label className="block text-sm">Seconds</label>
               <input 
                 type="text" 
                 value={customSeconds}
-                onChange={(e) => handleInputChange(e, setCustomSeconds)}
-                className=" p-2 w-16"
+                onChange={(e) => handleInputChange(e, setCustomSeconds, 59)}
+                className=" p-2 w-16 text-center rounded-md focus:outline-0 focus:shadow-md"
                 maxLength={2}
+                ref={secondsRef}
+                onFocus={() => secondsRef.current.select()} 
               />
             </div>
-
           </div>
         </div>
         
-        <div className="flex justify-end gap-2">
-          <button className="px-4 py-2 bg-green-500 text-white rounded" onClick={handleSetCustomTime}>
+        <div className="flex justify-center gap-2 w-full">
+          <button className="w-full px-4 py-2 border border-green-500 text-green-500 rounded-xs" onClick={handleSetCustomTime}>
             Set time
-          </button>
-          <button className="px-4 py-2 bg-gray-300 rounded" onClick={() => setIsSettingTime(false)}>
-            Cancel
           </button>
           <button
             onClick={() => {
-              setTimeLeft(0);
-              setIsRunning(false);
               setCustomHours('00');
               setCustomMinutes('00');
               setCustomSeconds('00');
             }}
-            className="px-4 py-2 bg-red-500 text-white rounded"
+            className="w-full px-4 py-2 border border-red-500 text-red-500 rounded-sx"
           >
             Reset
           </button>
         </div>
       </div>
       
-      <div className="w-1/7 m-auto">
-        <div className="flex flex-col gap-2">
+      <div className="2xl:w-1/7 xl:w-1/6 md:w-2/7 w-5/6 m-auto mt-0">
+        <div className="flex flex-col gap-2 text-center">
           <h1 className="cursor-default">Timer</h1>
           <PatternFormat 
             value={formattedHours + formattedMinutes + formattedSeconds}
@@ -167,24 +169,32 @@ function App() {
           />
 
         </div>
-        <button 
-          onClick={startTimer} 
-          className="mt-5 w-1/2  p-3 bg-green-700 rounded-xl hover:bg-green-600 active:bg-green-500 cursor-pointer"
-        >
-          {timerStarted ? 'Stop' : 'Start'}
-        </button>
-        
-        {timeLeft > 0 && (
-          <button
-            onClick={() => setIsRunning(!isRunning)}
-            className="ms-5 px-4 py-2 bg-blue-500 text-white rounded"
-          >
-            {isRunning ? 'Pause' : 'Resume'}
-          </button>
-        )}
-      </div>
-
-      
+        <div className="flex gap-2 mt-5 h-14">
+        {
+          (hours || minutes || seconds) ?
+            <button 
+              onClick={startTimer} 
+              className="w-full p-3 border border-green-700 text-green-700 rounded-xs hover:bg-green-600 active:bg-green-500 cursor-pointer"
+            >
+              {timerStarted ? 'Pause' : 'Start'}
+            </button> 
+            : ''
+        }
+        {
+          timeLeft > 0 && (
+            <button
+              onClick={() => {
+                setTimeLeft(0);
+                setIsRunning(false);
+              }}
+              className="px-4 py-2  border border-blue-500 text-blue-500 rounded-xs"
+            >
+              Cancel
+            </button>
+          )
+        }
+        </div>
+      </div>      
     </div>
   )
 }
